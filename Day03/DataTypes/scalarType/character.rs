00645ae7cfd45df2797b810289dcd77a5c645a1f
Nullbyte40420 <theabhilashsingh@gmail.com>
2026-09-01 20:56:45 +0530

    Character

---
+++ Day03/DataTypes/scalarType/character.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let c: char = 'z'; // this is a character literal with type annotation of char
+    let z = 'Z'; // this is a character literal with default type annotation of char
+
+    // character has only one possible value and it is used for representing a single character in rust programming language
+
+    // character has only one type notation and it is char and it can hold any unicode scalar value and it is 4 bytes in size
+    // means it is more than ASCII character 
+    // Unicode scalar values range from U+0000 to U+D7FF and U+E000 to U+10FFFF inclusive
