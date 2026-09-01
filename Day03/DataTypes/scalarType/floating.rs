01f814c38d63d7e39c9505eeedac3de7d4d5b06c
Nullbyte40420 <theabhilashsingh@gmail.com>
2026-09-01 20:18:53 +0530

    Floating

---
+++ Day03/DataTypes/scalarType/floating.rs
@@ -0,0 +1,16 @@
+fn main(){
+    let mut x = 2.0; // this is a floating point literal 
+
+    let u: f32 = 3.0; // this is a floating point literal with type annotation of f32
+
+    // types of floating point type anotation
+    let a: f32 = 2.0; // this is a floating point literal with type annotation of f32
+    let b: f64 = 2.0; // this is a floating point literal with type annotation of f64
+    // by default type anotation for floating point literals is f64 if not specified because of modern cpu
+
+    // all  floating point types are signed and can hold both positive and negative numbers
+    let c: f32 = -2.0; // this is a floating point literal with type annotation of f32
+
+    // floating number are represented according to IEEE 754 standard and they are not precise and can cause rounding errors
+    
+}
