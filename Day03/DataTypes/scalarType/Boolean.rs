e55f51f39bc79fedb34a285552dde687c468989b
Nullbyte40420 <theabhilashsingh@gmail.com>
2026-09-01 20:52:22 +0530

    Boolean

---
+++ Day03/DataTypes/scalarType/Boolean.rs
@@ -0,0 +1,6 @@
+fn main(){
+    let x:bool = true; // this is a boolean literal with type annotation of bool
+    let y = false; // this is a boolean literal with default type annotation of bool
+
+    // boolean has only two possible values true and false and they are used for logical operations and control flow in rust programming language
+}
