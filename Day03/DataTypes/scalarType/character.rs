355f0eb3f63dbd0d22a761e5c89961c1f910c1c1
Nullbyte40420 <theabhilashsingh@gmail.com>
2026-09-01 20:57:41 +0530

    Character

--- Day03/DataTypes/scalarType/character.rs
+++ Day03/DataTypes/scalarType/character.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // character used single quotes not double quotes 
     let c: char = 'z'; // this is a character literal with type annotation of char
     let z = 'Z'; // this is a character literal with default type annotation of char
 
@@ -7,3 +8,5 @@ fn main() {
     // character has only one type notation and it is char and it can hold any unicode scalar value and it is 4 bytes in size
     // means it is more than ASCII character 
     // Unicode scalar values range from U+0000 to U+D7FF and U+E000 to U+10FFFF inclusive
+
+}
